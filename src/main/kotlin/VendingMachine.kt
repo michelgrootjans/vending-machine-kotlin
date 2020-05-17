aa4b45e418a330b50fb9a19e766a6684dd180d5a
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-17 09:17:56 +0000

    green

--- src/main/kotlin/VendingMachine.kt
+++ src/main/kotlin/VendingMachine.kt
@@ -13,7 +13,7 @@ class VendingMachine(
         else VendingMachine(balance, coinReject + coin)
 
     fun pressButton1(): VendingMachine =
-        VendingMachine(-1.00, coinReject, dispenser + "cola", "THANK YOU")
+        VendingMachine(0.00, coinReject, dispenser + "cola", "THANK YOU")
 
     fun pressButton2(): VendingMachine =
         VendingMachine(-1.00, coinReject, dispenser + "chips", "THANK YOU")
