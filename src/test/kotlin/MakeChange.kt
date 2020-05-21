f0e0be891184cf8ebddc940df1c4843417161635
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-21 11:31:58 +0000

    green

---
+++ src/test/kotlin/MakeChange.kt
@@ -0,0 +1,18 @@
+import io.kotlintest.matchers.collections.shouldBeEmpty
+import io.kotlintest.matchers.collections.shouldContainAll
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+//MAKE CHANGE
+//As a vendor
+//I want customers to receive correct change
+//So that they will use the vending machine again
+//
+//When a product is selected that costs less than the amount of
+//money in the machine, then the remaining amount is placed in the coin return.
+
+class MakeChange : StringSpec({
+    "No change for normal sale"{
+        val machine=VendingMachine()
+    }
+})
