648cf58035f964580dfb82c3877aa12552fee290
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-21 15:10:43 +0000

    green

--- src/main/kotlin/CoinReject.kt
+++ src/main/kotlin/CoinReject.kt
@@ -1,9 +1,4 @@
 class CoinReject(val coins: List<Coin> = emptyList()) {
-    fun add(coin: Coin): CoinReject {
-        return CoinReject(coins + coin)
-    }
-
-    fun add(change: List<Coin>): CoinReject {
-        return CoinReject(coins + change)
-    }
+    fun add(coin: Coin): CoinReject = CoinReject(coins + coin)
+    fun add(change: List<Coin>): CoinReject = CoinReject(coins + change)
 }
