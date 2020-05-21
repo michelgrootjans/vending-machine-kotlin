7dca9f5598462cc4b2a442ee4a2b19ccb9952b06
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-21 15:05:14 +0000

    green

--- src/main/kotlin/CoinReject.kt
+++ src/main/kotlin/CoinReject.kt
@@ -4,10 +4,6 @@ class CoinReject(val coins: List<Coin> = emptyList()) {
     }
 
     fun difficultstuffWith(balance: Balance, product: Product): CoinReject {
-        val change = balance.changeFor(product.price)
-        if(balance.amount() > product.price) {
-            return add(change[0])
-        }
-        return CoinReject( coins  + balance.changeFor(product.price))
+        return CoinReject(coins + balance.changeFor(product.price))
     }
 }
