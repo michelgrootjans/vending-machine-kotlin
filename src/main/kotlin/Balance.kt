59a47686453ba491dc88cff0cfd3097ce75439b0
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-21 13:33:32 +0000

    green

--- src/main/kotlin/Balance.kt
+++ src/main/kotlin/Balance.kt
@@ -2,11 +2,19 @@ class Balance(private val balance: Double = 0.00, private val coins: List<Coin> 
 
     fun show(): String = "%.2f".format(balance)
     fun isSufficientFor(amount: Double): Boolean = amount <= balance
-    fun add(amount: Double, coin: Coin): Balance = Balance(balance + amount)
-    fun subtract(amount: Double): Balance = Balance(balance - amount)
+    fun add(amount: Double, coin: Coin): Balance = Balance(balance + amount, coins + coin)
     fun isEmpty(): Boolean {
         return balance == 0.00
     }
+    fun balanc(): Double = coins.sumByDouble { coin -> valueOf(coin) }
 
     fun amount(): Double = balance
+
+private fun valueOf(coin: Coin) = when (coin) {
+    nickel() -> 0.05
+    dime() -> 0.10
+    quarter() -> 0.25
+    else -> 0.00
+}
+
 }
