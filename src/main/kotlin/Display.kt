f83633e4718204e88eaedbd04c18e957e23ea514
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-21 13:12:34 +0000

    green

--- src/main/kotlin/Display.kt
+++ src/main/kotlin/Display.kt
@@ -1,22 +1,14 @@
-private fun defaultMessage(balance: Balance): String {
-    return if (balance.isEmpty()) "INSERT COIN"
-    else balance.show()
-}
-
 open class Display(val _show: (Balance) -> String = fun(balance: Balance): String = defaultMessage(balance)) {
 
     fun show(balance: Balance): String = _show(balance)
 
-    fun saleSuccessful(): Display {
-        return Display { balance -> "THANK YOU" }
-    }
+    fun default(): Display = Display()
 
-    fun saleFailed(product: Product): Display {
-        return Display { balance -> "PRICE %.2f".format(product.price) }
-    }
-    fun default(): Display {
-        return Display()
-    }
-
+    fun saleSuccessful(): Display = Display { balance -> "THANK YOU" }
+    fun saleFailed(product: Product): Display = Display { balance -> "PRICE %.2f".format(product.price) }
 }
 
+private fun defaultMessage(balance: Balance): String {
+    return if (balance.isEmpty()) "INSERT COIN"
+    else balance.show()
+}
