be0468e6783d94afe7510b42afff842bd19c24c5
Michel Grootjans <michel.grootjans@persgroep.net>
2020-05-21 15:05:05 +0000

    green

--- src/main/kotlin/CoinReject.kt
+++ src/main/kotlin/CoinReject.kt
@@ -8,6 +8,6 @@ class CoinReject(val coins: List<Coin> = emptyList()) {
         if(balance.amount() > product.price) {
             return add(change[0])
         }
-        return CoinReject(balance.changeFor(product.price))
+        return CoinReject( coins  + balance.changeFor(product.price))
     }
 }
